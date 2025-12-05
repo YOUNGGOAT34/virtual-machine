7b85e40ee918ec694c3fb2499dfaad9586d395e9
GOAT <lucasonyiego283@gmail.com>
2025-12-05 11:01:05 +0300

    defining the general purpose registers

--- definitions.h
+++ definitions.h
@@ -1,6 +1,10 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
+#include <stdio.h>
+#include <stdint.h>
+
+
 
 typedef unsigned char u8;
 typedef unsigned short int u16;
@@ -11,6 +15,27 @@ typedef char i8;
 typedef short int i16;
 typedef  int i32;
 typedef long int i64;
+
+typedef struct REGISTERS{
+     u16 AX;
+     u16 BX;
+     u16 CX;
+     u16 DX;
+     u16 SP;
+     u16 IP;
+};
+
+typedef struct CPU{
+   REGISTERS Registers;
+
+};
+
+typedef struct VM{
+    CPU cpu;
+     
+};
+
+
 
 
 #endif
